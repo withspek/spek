baa663292374bced3ee68cf3312b3f4a354b795d
Irere123 <irere2020@outlook.com>
2024-03-03 19:40:30 +0200

    direct messages page

---
+++ apps/web/src/app/(authenticated)/direct/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Direct",
+};
+
+export default function DirectMessagesPage() {
+  return (
+    <div>
+      <p>Direct messages</p>
+    </div>
+  );
+}
