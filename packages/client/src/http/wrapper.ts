8b4eddf6f7b6ff2e9c48e263409f624c985acb93
Irere123 <irere2020@outlook.com>
2024-03-08 17:28:27 +0200

    feat: create community wrapper function

--- packages/client/src/http/wrapper.ts
+++ packages/client/src/http/wrapper.ts
@@ -1,9 +1,15 @@
-import { User } from "../entities";
+import { Community, User } from "../entities";
 import { Connection } from "./raw";
 
 export const wrap = (connection: Connection) => ({
   query: {
     getUsers: (): Promise<User[]> => connection.send("/dev", "GET"),
   },
-  mutation: {},
+  mutation: {
+    createCommunity: (data: {
+      name: string;
+      description: string;
+    }): Promise<Community> =>
+      connection.send("/community/create", "POST", { ...data }),
+  },
 });
