392574fe293277fa0c3707b4e50d21236299f00b
Irere123 <irere2020@outlook.com>
2024-03-08 05:36:57 +0200

    feat: input component

--- apps/web/src/app/(authenticated)/new/community/page.tsx
+++ apps/web/src/app/(authenticated)/new/community/page.tsx
@@ -1,7 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "New Community",
+};
+
 export default function NewCommunityPage() {
   return (
-    <div>
-      <p>Hello world</p>
+    <div className="flex w-full h-full flex-1 justify-center items-center">
+      <div>
+        <h2 className="text-xl font-bold">Create a new community</h2>
+        <p>A community is a place for you to talk and share ideas as a whole</p>
+      </div>
     </div>
   );
 }
