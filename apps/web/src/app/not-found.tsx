bed824c2d985b081ce90e839ad935f740d095ab0
Irere123 <irere2020@outlook.com>
2024-03-08 18:00:47 +0200

    feat: not-found page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/" className="underline text-alabaster-600">
+        {" "}
+        Return Home
+      </Link>
+    </div>
+  );
+}
