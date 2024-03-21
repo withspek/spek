09f4c71bb9117b0a237caf953a12f433e7871271
Irere123 <irere2020@outlook.com>
2024-03-21 18:05:02 +0200

    feat: user session and user session supervisor

--- api/lib/spek/user_session.ex
+++ api/lib/spek/user_session.ex
@@ -6,81 +6,114 @@ defmodule Spek.UserSesion do
               pid: nil
   end
 
-  def start_link(%State{
-        user_id: user_id
-      }) do
-    GenServer.start_link(
-      __MODULE__,
-      %State{
-        pid: nil,
-        user_id: user_id
-      },
-      name: :"#{user_id}:user_session"
-    )
-  end
+  #################################################################################
+  # REGISTRY AND SUPERVISION BOILERPLATE
 
-  def init(x) do
-    {:ok, x}
-  end
+  defp via(user_id), do: {:via, Registry, {Spek.UserSesionRegistery, user_id}}
 
-  def send_call!(user_id, params) do
-    case send_call(user_id, params) do
-      {:ok, x} ->
-        x
+  defp cast(user_id, params), do: GenServer.cast(via(user_id), params)
+  defp call(user_id, params), do: GenServer.call(via(user_id), params)
 
-      _ ->
-        nil
+  def start_supervised(initial_values) do
+    callers = [self() | Process.get(:"$callers", [])]
+
+    case DynamicSupervisor.start_child(
+           Spek.UserSesionDynamicSupervisor,
+           {__MODULE__, Keyword.merge(initial_values, callers: callers)}
+         ) do
+      {:error, {:already_started, pid}} -> {:ignored, pid}
+      error -> error
     end
   end
 
-  def send_call(user_id, params) do
-    case GenRegistry.looup(Spek.UserSesion, user_id) do
-      {:ok, session} ->
-        GenServer.cast(session, params)
+  def child_spec(init), do: %{super(init) | id: Keyword.get(init, :user_id)}
 
-      err ->
-        err
-    end
+  def count, do: Registry.count(Spek.UserSesionRegistery)
+
+  ###############################################################################
+  ## INITIALIZATION BOILERPLATE
+
+  def start_link(init) do
+    GenServer.start_link(__MODULE__, init, name: via(init[:user_id]))
   end
 
-  def handle_cast({:set, key, value}, state) do
+  def init(init) do
+    # transfer callers into the running process.
+    Process.put(:"$callers", Keyword.get(init, :callers))
+    {:ok, struct(State, init)}
+  end
+
+  ##############################################################################
+  ## API HOOKS
+  ## TODO: CHANGE CASTS TO CALLS
+
+  def set(user_id, key, value), do: cast(user_id, {:set, key, value})
+
+  defp set_impl(key, value, state) do
     {:noreply, Map.put(state, key, value)}
   end
 
-  def handle_cast({:send_ws_msg, _platform, msg}, state) do
-    if not is_nil(state.pid) do
-      send(state.pid, {:remote_send, msg})
-    end
+  def send_ws(user_id, platfrom, msg), do: cast(user_id, {:send_ws, platfrom, msg})
 
+  defp send_ws_impl(_platform, msg, state = %{pid: pid}) do
+    if pid, do: send(pid, {:remote_send, msg})
     {:noreply, state}
   end
 
-  def handle_cast({:new_tokens, tokens}, state) do
-    if not is_nil(state.pid) do
-      send(state.pid, {:remote_send, %{op: "new-tokens", d: tokens}})
-    end
+  def new_tokens(user_id, tokens), do: cast(user_id, {:new_tokens, tokens})
 
+  def new_tokens_impl(tokens, state = %{pid: pid}) do
+    if pid, do: send(pid, {:remote_send, %{op: "new-tokens", d: tokens}})
     {:noreply, state}
   end
 
-  def handle_call({:set_pid, pid}, _from, state) do
-    if not is_nil(state.pid) do
+  def set_state(user_id, info), do: cast(user_id, {:set_state, info})
+
+  defp set_state_impl(info, state) do
+    {:noreply, Map.merge(state, info)}
+  end
+
+  def get(user_id, key), do: call(user_id, {:get, key})
+
+  defp get_impl(key, _reply, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  def set_pid(user_id, pid), do: call(user_id, {:set_pid, pid})
+
+  defp set_pid(pid, _reply, state) do
+    if state.pid do
       send(state.pid, {:kill})
     else
-      # TODO: Set that the user is online
+      # TODO: SET ONLINE STATUS
     end
 
     Process.monitor(pid)
     {:reply, :ok, %{state | pid: pid}}
   end
 
-  def handle_info({:DOWN, _ref, :process, pid, _reason}, state) do
-    if state.pid === pid do
-      # TODO: SET USER OFFLINE
+  ##############################################################################
+  ## MESSAGING API.
 
-      {:stop, :normal, state}
-    else
-      {:noreply, state}
-    end
+  defp handle_disconnect(pid, state = %{pid: pid}) do
+    # TODO: SET OFFLINE STATUS
+
+    {:stop, :normal, state}
   end
+
+  defp handle_disconnect(_, state), do: {:noreply, state}
+
+  #############################################################################
+  ## ROUTER
+
+  def handle_cast({:set, key, value}, state), do: set_impl(key, value, state)
+
+  def handle_cast({:send_ws, platform, msg}, state), do: send_ws_impl(platform, msg, state)
+  def handle_cast({:new_tokens, tokens}, state), do: new_tokens_impl(tokens, state)
+  def handle_cast({:set_state, info}, state), do: set_state_impl(info, state)
+
+  def handle_call({:get, key}, reply, state), do: get_impl(key, reply, state)
+  def handle_call({:set_pid, pid}, reply, state), do: set_pid(pid, reply, state)
+
+  def handle_info({:DOWN, _ref, :process, pid, _reason}, state), do: handle_disconnect(pid, state)
 end
