4971fe8cb5864f726a03ffa224f8aa4651d17368
Irere123 <irere2020@outlook.com>
2024-03-28 11:43:57 +0200

    config api

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -17,6 +17,26 @@ config :spek, OAuth.Gitlab,
     System.get_env("GITLAB_REDIRECT_URI") ||
       raise("GITLAB_REDIRECT_URI not set")
 
+config :spek, OAuth.Github,
+  client_id:
+    System.get_env("GITHUB_APP_ID") ||
+      raise("""
+      environment variable GITHUB_APP_ID not set.
+      Create an oauth application on gitlab to get one
+      """),
+  client_secret:
+    System.get_env("GITHUB_APP_SECRET") ||
+      raise("""
+      environment variable GITLAB_APP_SECRET not set.
+      Create an oauth application on gitlab to get one
+      """),
+  redirect_uri:
+    System.get_env("GITHUB_REDIRECT_URI") ||
+      raise("""
+      environment variable GITHUB_REDIRECT_URI not set.
+      set it your os environment
+      """)
+
 config :spek,
   web_url: "http://localhost:3000",
   api_url: "http://localhost:4001",
