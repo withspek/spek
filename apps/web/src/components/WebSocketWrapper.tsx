745f8d524a95f3e9951a6937189c45e944fa220f
Irere123 <irere2020@outlook.com>
2024-04-05 18:10:47 +0200

    fix: connect to ws when has tokens

--- apps/web/src/components/WebSocketWrapper.tsx
+++ apps/web/src/components/WebSocketWrapper.tsx
@@ -1,16 +1,16 @@
 import { WebSocketProvider } from "@/contexts/WebSocketContext";
-import { useConn } from "@/hooks/useConn";
 import { MainWsHandlerProvider } from "@/hooks/useMainWsHandler";
+import { useTokenStore } from "@/stores/useTokenStore";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const WebSocketWrapper: React.FC<Props> = ({ children }) => {
-  const { user } = useConn();
+  const hasTokens = useTokenStore((s) => !!(s.accessToken && s.refreshToken));
 
   return (
-    <WebSocketProvider shouldConnect={user ? true : false}>
+    <WebSocketProvider shouldConnect={hasTokens}>
       <MainWsHandlerProvider>{children}</MainWsHandlerProvider>
     </WebSocketProvider>
   );
