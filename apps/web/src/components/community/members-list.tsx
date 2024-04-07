23b0318ced495a3029110ad5da2ee8063c2d8340
Irere123 <irere2020@outlook.com>
2024-04-07 19:28:41 +0200

    fix(ui): display community members properly

--- apps/web/src/components/community/members-list.tsx
+++ apps/web/src/components/community/members-list.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import { useTypeSafeQuery } from "@/hooks/useTypeSafeQuery";
 import { useRouter } from "next/navigation";
+import { Avatar } from "@/ui/avatar";
+import { useConn } from "@/hooks/useConn";
 
 interface MembersListProps {
   communityId: string;
 }
 
 export const MembersList: React.FC<MembersListProps> = ({ communityId }) => {
+  const { user } = useConn();
   const { push } = useRouter();
   const { data, isLoading } = useTypeSafeQuery(
     ["getCommunityMembers", communityId],
@@ -23,14 +26,19 @@ export const MembersList: React.FC<MembersListProps> = ({ communityId }) => {
     <div className="flex flex-col gap-4">
       {data?.map((m) => (
         <div
-          className="cursor-pointer"
+          className="flex gap-4 items-center cursor-pointer"
           key={m.id}
           onClick={() => {
             push(`/u/${m.id}`);
           }}
         >
-          <img src={m.avatarUrl} alt={m.username} />
-          <p>{m.displayName}</p>
+          <Avatar src={m.avatarUrl} size="sm" username={m.username} />
+          <div>
+            <p>
+              {m.displayName} {user && user.id === m.id && <span>(you)</span>}
+            </p>
+            <p className="text-alabaster-400 text-balance">{m.bio}</p>
+          </div>
         </div>
       ))}
     </div>
