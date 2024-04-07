8bc28f1106b438b63356133b36809baf030b053c
Irere123 <irere2020@outlook.com>
2024-04-07 18:03:56 +0200

    feat(api): get user info with thread sub info

--- api/lib/db_operations/access/users.ex
+++ api/lib/db_operations/access/users.ex
@@ -1,14 +1,23 @@
 defmodule Operations.Access.Users do
   import Ecto.Query
+
+  alias Operations.Queries.Users, as: Query
   alias Spek.Repo
   alias Models.User
 
   def get_user_id(user_id) do
-    from(u in User, where: u.id == ^user_id, limit: 1) |> Repo.one()
+    Query.start()
+    |> Query.filter_by_id(user_id)
+    |> Query.thread_sub_info(user_id)
+    |> Query.limit_one()
+    |> Repo.one()
   end
 
   def get_by_username(username) do
-    from(u in User, where: u.username == ^username, limit: 1) |> Repo.one()
+    Query.start()
+    |> Query.filter_by_username(username)
+    |> Query.limit_one()
+    |> Repo.one()
   end
 
   def search_username(<<first_letter>> <> rest) when first_letter == ?@ do
@@ -25,10 +34,7 @@ defmodule Operations.Access.Users do
   end
 
   def get_users do
-    query =
-      from(u in User)
-      |> Repo.all([])
-
-    query
+    Query.start()
+    |> Repo.all()
   end
 end
