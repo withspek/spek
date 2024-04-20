81c8903acbde74c011c03643c456dcb5ed31db5c
Irere123 <irere2020@outlook.com>
2024-04-20 11:57:28 +0200

    fix: deploy action script command

--- api/lib/routes/community.ex
+++ api/lib/routes/community.ex
@@ -10,7 +10,7 @@ defmodule Routes.Community do
   plug(Plugs.CheckAuth, %{shouldThrow: false})
   plug(:dispatch)
 
-  # TODO: fix warning
+  # TODO: fix warning after
   get "/all" do
     communities = Communities.get_top_communities(40)
 
