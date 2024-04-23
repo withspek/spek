59af11b425549c88dec42483edaad7c724f61d7b
Irere123 <irere2020@outlook.com>
2024-04-23 08:33:19 +0200

    fix: refetch user dms on mount

--- apps/web/src/app/(authenticated)/direct/controller.tsx
+++ apps/web/src/app/(authenticated)/direct/controller.tsx
@@ -4,19 +4,16 @@ import { ConversationsList } from "@/components/direct/ConversationsList";
 import { useTypeSafeQuery } from "@/hooks/useTypeSafeQuery";
 
 export const DirectMessagePageController: React.FC = ({}) => {
-  const { data, isLoading } = useTypeSafeQuery("getUserDms", {
-    staleTime: Infinity,
-    refetchOnMount: "always",
-  });
+  const { data, isLoading } = useTypeSafeQuery("getUserDms");
 
-  if (isLoading) {
-    return null;
+  if (isLoading || !data) {
+    return <div>Loading...</div>;
   }
 
   return (
     <>
       <h2>Directs</h2>
-      <ConversationsList conversations={data!} />
+      <ConversationsList conversations={data} />
     </>
   );
 };
