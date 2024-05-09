d248065d0e9814973cb26b1ed56a478e6ba01938
Irere123 <irere2020@outlook.com>
2024-05-09 10:28:41 +0200

    refatcor: rename tailwindconfig to config

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,5 +1,5 @@
 // tailwind config is required for editor support
-import sharedConfig from "tailwind-config/tailwind.config";
+import sharedConfig from "config/tailwind.config";
 import type { Config } from "tailwindcss";
 
 const config: Pick<Config, "presets"> = {
