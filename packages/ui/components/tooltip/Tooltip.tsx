e8d716f131a89cc7771474a6d38cab99a1963a63
Irere123 <irere2020@outlook.com>
2024-05-11 08:58:25 +0200

    feat(ui): Tooltip component

---
+++ packages/ui/components/tooltip/Tooltip.tsx
@@ -0,0 +1,53 @@
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import React from "react";
+
+import { classNames } from "@spek/lib";
+
+export function Tooltip({
+  children,
+  content,
+  open,
+  defaultOpen,
+  onOpenChange,
+  delayDuration,
+  side = "top",
+  ...props
+}: {
+  children: React.ReactNode;
+  content: React.ReactNode;
+  delayDuration?: number;
+  open?: boolean;
+  defaultOpen?: boolean;
+  side?: "top" | "right" | "bottom" | "left";
+  onOpenChange?: (open: boolean) => void;
+} & TooltipPrimitive.TooltipContentProps) {
+  const Content = (
+    <TooltipPrimitive.Content
+      {...props}
+      className={classNames(
+        "calcom-tooltip",
+        side === "top" && "-mt-7",
+        side === "right" && "ml-2",
+        "bg-primary-800 text-primary-50 relative z-50 rounded-md px-2 py-1 text-xs font-semibold shadow-lg",
+        props.className && `${props.className}`
+      )}
+      side={side}
+      align="center"
+    >
+      {content}
+    </TooltipPrimitive.Content>
+  );
+
+  return (
+    <TooltipPrimitive.Root
+      delayDuration={delayDuration || 50}
+      open={open}
+      defaultOpen={defaultOpen}
+      onOpenChange={onOpenChange}
+    >
+      <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+    </TooltipPrimitive.Root>
+  );
+}
+
+export default Tooltip;
