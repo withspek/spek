545dcbd9f864bdb41195efa8394aa0a5a3881afb
Irere123 <irere2020@outlook.com>
2024-05-11 09:21:10 +0200

    constants

---
+++ packages/lib/constants.ts
@@ -0,0 +1,10 @@
+export const AVATAR_FALLBACK = "/avatar.svg";
+export const PROD = process.env.NODE_ENV === "production";
+
+export const WEBAPP_URL = PROD
+  ? "https://spek.vercel.app"
+  : "http://localhost:3000";
+
+export const API_URL = PROD
+  ? "https://spek-latest.onrender.com"
+  : "http://localhost:4001";
