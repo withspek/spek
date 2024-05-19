6b045574de38a5773029e1b4df0857c2d5975d2b
Irere123 <irere2020@outlook.com>
2024-05-19 14:04:05 +0200

    feat: blog vercel analytics

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
     site: 'https://example.com',
     integrations: [
         mdx(),
@@ -12,5 +14,8 @@ export default defineConfig({
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    })
 });
