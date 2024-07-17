2d626227afc360d178f7f0324f109a18b5b50106
Irere123 <irere2020@outlook.com>
2024-07-17 19:41:41 +0200

    not found page

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFoundPage() {
   return (
-    <div>
+    <div className="flex w-full h-full text-center justify-center items-center">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/" className="underline text-alabaster-600">
