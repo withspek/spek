0d4d38fc84fca5e8d7726ef93f6a72d3aa001d9f
Irere123 <irere2020@outlook.com>
2024-07-23 12:32:57 +0200

    feat(api): health check route

--- api/lib/breeze/routes/health-check.ex
+++ api/lib/breeze/routes/health-check.ex
@@ -1,18 +1,18 @@
 defmodule Breeze.Routes.HealthCheck do
   use Plug.Router
 
-  alias Breeze.Plugs
-
-  plug(Plugs.Cors)
   plug(:match)
   plug(:dispatch)
 
   get "/" do
-    send_resp(conn, "ok", 200)
+    resp = %{success: true}
+
+    conn
+    |> send_resp(200, Jason.encode!(resp))
   end
 
   match _ do
     conn
-    |> send_resp(404, "not found")
+    |> send_resp(404, "Not found")
   end
 end
