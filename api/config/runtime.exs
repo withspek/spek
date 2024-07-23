5bda79da091524d8932c7033a383a888919ddc6e
Irere123 <irere2020@outlook.com>
2024-07-23 15:04:49 +0200

    feat(api): ssl options

--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -15,23 +15,7 @@ if config_env() == :prod do
       For example: ecto://USER:PASSWORD@HOST:PORT/DATABASE
       """
 
-  %URI{host: database_host} = URI.parse(database_url)
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
-
-  config :spek, Telescope.Repo,
-    url: database_url,
-    # Our production database requires SSL to be enabled to connect.
-    ssl: true,
-    ssl_opts: [
-      verify: :verify_none,
-      server_name_indication: to_charlist(database_host),
-      customize_hostname_check: [
-        match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-      ]
-    ],
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+  config :spek, Telescope.Repo, url: database_url
 
   config :spek, Breeze.OAuth.Gitlab,
     client_id:
