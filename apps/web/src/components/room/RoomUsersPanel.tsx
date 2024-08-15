cd11bf9327742228f86a1ed5fec201fa652b4513
Irere123 <irere2020@outlook.com>
2024-08-15 16:18:43 +0200

    feat(horizon): deploy horizon

--- apps/web/src/components/room/RoomUsersPanel.tsx
+++ apps/web/src/components/room/RoomUsersPanel.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Conf, ConfUser } from "@spek/client";
 
 import { useSplitUsersIntoSections } from "./useSplitUsersIntoSections";
+import { Badge } from "@spek/ui";
 
 interface Props {
   conf: Conf;
@@ -15,12 +16,27 @@ export const RoomUsersPanel: React.FC<Props> = (props) => {
     useSplitUsersIntoSections(props);
 
   return (
-    <div>
-      {speakers.length ? <p>Speakers</p> : null}
-      {speakers}
-      {askingToSpeak}
-      {listeners.length ? <p>Listeners</p> : null}
-      {listeners}
+    <div className={`flex pt-4 px-4 flex-1`}>
+      <div className="w-full block">
+        <div className={`w-full grid grid-cols-5 gap-5`}>
+          {speakers}
+          {askingToSpeak.length ? (
+            <div className="flex gap-2 col-span-full h-4">
+              <p>Asking to speak</p>
+              <Badge>{askingToSpeak.length}</Badge>
+            </div>
+          ) : null}
+          {askingToSpeak}
+          {listeners.length ? (
+            <div className="flex gap-2 col-span-full h-4">
+              <p>Listeners</p>
+              <Badge>{listeners.length}</Badge>
+            </div>
+          ) : null}
+          {listeners}
+          <div className={`flex h-3 w-full col-span-full`}></div>
+        </div>
+      </div>
     </div>
   );
 };
